61a68af475294926d908d08fc7a9e14191318240
Eduardo Barbz <eduardob907@gmail.com>
2022-01-28 18:00:45 +0100

    :lock: add security

---
+++ subscription-sys-core/src/main/java/dev/barbz/subscriptionsyscore/infrastructure/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package dev.barbz.subscriptionsyscore.infrastructure.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        // XSS (Cross-Site Scripting) protection
+        http
+                .headers()
+                .xssProtection()
+                .and()
+                .contentSecurityPolicy("script-src 'self'");
+    }
+
+    @Override
+    public void configure(WebSecurity web) {
+        // Disable auth security for all request
+        web.ignoring().antMatchers("/**");
+    }
+}
