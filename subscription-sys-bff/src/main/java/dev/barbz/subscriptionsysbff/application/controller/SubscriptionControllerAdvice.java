a426a0b612f6aae7595820b1dddba94457cf6955
Eduardo Barbz <eduardob907@gmail.com>
2022-01-28 20:45:59 +0100

    :rocket: log web client response error

--- subscription-sys-bff/src/main/java/dev/barbz/subscriptionsysbff/application/controller/SubscriptionControllerAdvice.java
+++ subscription-sys-bff/src/main/java/dev/barbz/subscriptionsysbff/application/controller/SubscriptionControllerAdvice.java
@@ -1,14 +1,20 @@
 package dev.barbz.subscriptionsysbff.application.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.barbz.subscriptionsysbff.application.response.ErrorResponse;
-import dev.barbz.subscriptionsysbff.domain.SubscriptionException;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.reactive.function.client.WebClientResponseException;
 
+import static java.util.Objects.nonNull;
+
+@Slf4j
 @ControllerAdvice
 public record SubscriptionControllerAdvice() {
 
@@ -21,10 +27,20 @@ public record SubscriptionControllerAdvice() {
     }
 
     @ResponseBody
-    @ExceptionHandler(SubscriptionException.class)
-    public ResponseEntity<ErrorResponse> handleSubscriptionException(SubscriptionException e) {
-        return ResponseEntity
-                .status(HttpStatus.BAD_REQUEST)
-                .body(new ErrorResponse(e.getMessage(), e.getDetailedMessage()));
+    @ExceptionHandler(WebClientResponseException.class)
+    public ResponseEntity<ErrorResponse> handleWebClientResponseException(WebClientResponseException e) {
+        try {
+            ErrorResponse errorResponse = new ObjectMapper().readValue(e.getResponseBodyAsString(), ErrorResponse.class);
+            if (nonNull(e.getRequest())) {
+                log.error("{}: {}  |  {}", e.getRequest().getMethod(), e.getRequest().getURI(), errorResponse.detailedMessage());
+            }
+            return ResponseEntity
+                    .status(e.getRawStatusCode())
+                    .body(errorResponse);
+        } catch (JsonProcessingException exc) {
+            return ResponseEntity
+                    .status(e.getRawStatusCode())
+                    .body(new ErrorResponse("SERVICE_ERROR", e.getResponseBodyAsString()));
+        }
     }
 }
