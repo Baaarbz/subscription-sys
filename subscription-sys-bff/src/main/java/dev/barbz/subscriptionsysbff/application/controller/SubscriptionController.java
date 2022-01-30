fe31da54796bd2ad388cb75c46e170bcdb41b630
Eduardo Barbz <eduardob907@gmail.com>
2022-01-30 12:39:00 +0100

    :rocket: documented with swagger

--- subscription-sys-bff/src/main/java/dev/barbz/subscriptionsysbff/application/controller/SubscriptionController.java
+++ subscription-sys-bff/src/main/java/dev/barbz/subscriptionsysbff/application/controller/SubscriptionController.java
@@ -1,10 +1,19 @@
 package dev.barbz.subscriptionsysbff.application.controller;
 
 import dev.barbz.subscriptionsysbff.application.request.RegisterSubscriptionRequest;
+import dev.barbz.subscriptionsysbff.application.response.ErrorResponse;
 import dev.barbz.subscriptionsysbff.application.response.SubscriptionResponse;
 import dev.barbz.subscriptionsysbff.domain.service.SubscriptionService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.headers.Header;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,28 +27,74 @@ import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.List;
 
-@RestController
-@RequestMapping("v1/subscriptions")
+/**
+ * Subscription controller.
+ * Definition of the API.
+ */
 @Slf4j
+@RestController
+@RequestMapping(path = "v1/subscriptions", produces = MediaType.APPLICATION_JSON_VALUE)
+@Tag(name = "Subscription", description = "Subscription API with the available operations.")
 public record SubscriptionController(SubscriptionService subscriptionService) {
 
+    /**
+     * Retrieve all the subscriptions registered.
+     *
+     * @return list of subscriptions.
+     */
     @GetMapping
+    @Operation(summary = "Get all subscriptions.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found subscriptions."),
+            @ApiResponse(responseCode = "204", description = "There are not subscriptions.", content = @Content),
+    })
     public ResponseEntity<List<SubscriptionResponse>> retrieveAll() {
         log.info("GET\t- list of subscriptions");
         List<SubscriptionResponse> response = subscriptionService.retrieveAllSubscriptions();
+        if (response.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
         return ResponseEntity.ok(response);
     }
 
+    /**
+     * Retrieve subscription by its id.
+     *
+     * @param subscriptionId subscription id.
+     * @return requested subscription.
+     */
+    @Operation(summary = "Get subscription by its id.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Found subscription by id."),
+            @ApiResponse(responseCode = "404", description = "Subscription not found.",
+                    content = @Content(schema = @Schema(implementation = ErrorResponse.class))),
+    })
     @GetMapping("{subscriptionId}")
     public ResponseEntity<SubscriptionResponse> retrieve(
-            @Parameter(description = "ID of the requested subscription", example = "61f4588813a91001bf1bf486")
+            @Parameter(description = "ID of the requested subscription.", example = "61f4588813a91001bf1bf486")
             @PathVariable String subscriptionId) {
         log.info("GET\t- subscription {}", subscriptionId);
         SubscriptionResponse response = subscriptionService.retrieveSubscription(subscriptionId);
         return ResponseEntity.ok(response);
     }
 
-    @PostMapping("register")
+    /**
+     * Register a new subscription in the system, once the process is finished the user will receive a notification
+     * in his mail inbox.
+     *
+     * @param subscriptionRequest subscription request body.
+     * @return location header with the relative path of the registered subscription.
+     * @throws URISyntaxException URI syntax exception.
+     */
+    @Operation(summary = "Register a new subscription in the system.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "201", description = "Subscription registered successfully ", content = @Content,
+                    headers = {@Header(name = "Location",
+                            description = "Indicates the relative path to retrieve the create subscription")}),
+            @ApiResponse(responseCode = "400", content = @Content(schema = @Schema(implementation = ErrorResponse.class)),
+                    description = "Invalid body, can be caused by fields with empty/nulls values, wrong formats"),
+    })
+    @PostMapping(path = "register", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Void> register(@RequestBody RegisterSubscriptionRequest subscriptionRequest) throws URISyntaxException {
         log.info("POST\t- register new subscription");
         String id = subscriptionService.registerSubscription(subscriptionRequest);
@@ -48,6 +103,18 @@ public record SubscriptionController(SubscriptionService subscriptionService) {
         return ResponseEntity.created(resource).build();
     }
 
+    /**
+     * Remove requested subscription of the system.
+     *
+     * @param subscriptionId subscription id.
+     * @return void.
+     */
+    @Operation(summary = "Remove requested subscription of the system.")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Removed subscription", content = @Content),
+            @ApiResponse(responseCode = "404", content = @Content(schema = @Schema(implementation = ErrorResponse.class)),
+                    description = "Subscription to remove not found."),
+    })
     @DeleteMapping("{subscriptionId}/cancel")
     public ResponseEntity<Void> cancel(
             @Parameter(description = "ID of the subscription to delete", example = "61f4588813a91001bf1bf486")
--- subscription-sys-bff/src/main/java/dev/barbz/subscriptionsysbff/application/response/SubscriptionResponse.java
+++ subscription-sys-bff/src/main/java/dev/barbz/subscriptionsysbff/application/response/SubscriptionResponse.java
@@ -1,16 +1,26 @@
 package dev.barbz.subscriptionsysbff.application.response;
 
 import dev.barbz.subscriptionsysbff.domain.enums.Gender;
+import io.swagger.v3.oas.annotations.media.Schema;
 
 import java.time.LocalDate;
 
+@Schema(description = "Subscriber saved data in the system.")
 public record SubscriptionResponse(
+        @Schema(description = "Subscription id.", example = "61f4588813a91001bf1bf486")
         String id,
+        @Schema(description = "Subscriber first name.", example = "John")
         String firstName,
+        @Schema(description = "Subscriber last name.", example = "Cena")
         String lastName,
+        @Schema(description = "Mail to send the notification.", example = "john.cena@email.com")
         String email,
+        @Schema(description = "Subscriber gender to, in case of needed, chose the right treat in the mail.", example = "MALE")
         Gender gender,
+        @Schema(description = "Birthday", example = "2000-12-31", format = "yyyy-MM-dd")
         LocalDate birthday,
+        @Schema(description = "Explicit consent to send mails", example = "true")
         Boolean consent,
+        @Schema(description = "Campaign which is subscribed", example = "adidas-campaign")
         String campaign) {
 }
