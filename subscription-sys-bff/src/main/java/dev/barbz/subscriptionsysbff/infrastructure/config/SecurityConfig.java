1b0c151f02d837c9f9c5c1040a41311117c0e82b
Eduardo Barbz <eduardob907@gmail.com>
2022-02-01 14:05:23 +0100

    :rocket: add cors and update security config

--- subscription-sys-bff/src/main/java/dev/barbz/subscriptionsysbff/infrastructure/config/SecurityConfig.java
+++ subscription-sys-bff/src/main/java/dev/barbz/subscriptionsysbff/infrastructure/config/SecurityConfig.java
@@ -1,14 +1,14 @@
 package dev.barbz.subscriptionsysbff.infrastructure.config;
 
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * Security configuration class.
  */
-@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     /**
--- subscription-sys-core/src/main/java/dev/barbz/subscriptionsyscore/infrastructure/config/SecurityConfig.java
+++ subscription-sys-core/src/main/java/dev/barbz/subscriptionsyscore/infrastructure/config/SecurityConfig.java
@@ -1,18 +1,20 @@
 package dev.barbz.subscriptionsyscore.infrastructure.config;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
 
 /**
  * Security configuration class.
  */
-@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     /**
-     * Define XSS (Cross-Site Scripting) protection.
+     * Define XSS (Cross-Site Scripting) protection, and define CORS protection.
      *
      * @param http http security object
      * @throws Exception generic exception
@@ -20,6 +22,19 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors()
+                .configurationSource(request -> {
+                    CorsConfiguration corsConfiguration = new CorsConfiguration();
+                    // Set allowed methods
+                    corsConfiguration.addAllowedMethod(HttpMethod.GET);
+                    corsConfiguration.addAllowedMethod(HttpMethod.POST);
+                    corsConfiguration.addAllowedMethod(HttpMethod.DELETE);
+                    // Set allowed origins
+                    corsConfiguration.addAllowedOrigin("http://localhost:8080");
+                    corsConfiguration.addAllowedOrigin("http://subscription-sys-bff:8080");
+                    return corsConfiguration;
+                })
+                .and()
                 .headers()
                 .xssProtection()
                 .and()
